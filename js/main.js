73ee0e789692419f53908d0f0d02c36ce795e1fe
mindgod <dgrainys@gmail.com>
2017-05-29 17:58:01 +0300

    Test

--- js/main.js
+++ js/main.js
@@ -3,9 +3,26 @@ for (var i = 0; i < img.length; i++){
 	img[i].addEventListener('click', doSelect);
 }
 function doSelect(){
-	this.classList.add('select');
+	if (this.classList.contains('select')) {
+		this.classList.remove('select');
+	} else {
+	this.classList.add('select')
+	}
+    
+var select = document.getElementsByClassName('select').length;
+document.getElementsByClassName('demo')[0].innerHTML = "You have selected " + select + " images";
 }
-var select = document.getElementsByClassName('select');
-var count = select.length;
-console.log(select);
-// document.write('You have selected ' + count + ' images');
+var atag = document.getElementsByTagName('a');
+
+function removeElement() {
+    this.parentNode.remove();
+}
+
+for (var y = 0; y < atag.length; y++){
+	atag[y].addEventListener('click', removeElement);
+}
+
+
+
+
+
